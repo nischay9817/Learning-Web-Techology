c562aa1fdeb8afb861c62de874725fb1743b24d2
nischayshrestha <nikechrestha@gmail.com>
2025-07-17 09:12:58 +0545

    day1 of learning  js

---
+++ day1.js
@@ -0,0 +1,16 @@
+// if (true){
+//     let name="Nischay Shrestha"
+//     console.log(name);
+// }
+// let name="Nischay"
+// console.log(name+"Shrestha")
+
+// Array
+// let friends=["Nischay","Shrestha","Ram"]
+// console.log(friends[0])
+// Object
+let khoyo={
+    Name: "Nischay Shrestha",
+    reactCoursePrice: 999,
+};
+console.log(khoyo.Name)
